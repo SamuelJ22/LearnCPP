8bceaea854e8f804ace9f8d382c577a53b6d4cc6
SamuelJ22 <Samueljadhav1@gmail.com>
2021-05-18 19:59:13 +0530

    prime number

---
+++ primenumber.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j, total, arr[100], k;
+    cout << "Enter size of array " << endl;
+    cin >> total;
+    cout << "Enter " << total << " number of array elements" << endl;
+    for (i = 0; i < total; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "The prime numbers from array are:-";
+    for (i = 0; i < total; i++)
+    {
+        j = 2;
+        k = 1;
+        while(j < arr[i])
+        {
+            if (arr[i] % j == 0)
+                k = 0;
+                
+         j++;
+        }
+        if (k == 1)
+        {
+            cout << arr[i] << " , ";
+        }
+    }
+
+    return 0;
+}
