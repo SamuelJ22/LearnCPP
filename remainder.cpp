be2900cca1c52e6c59f1422f611dfe9c0db7a811
SamuelJ22 <Samueljadhav1@gmail.com>
2021-05-21 17:21:43 +0530

    quetient&remainder commit

---
+++ remainder.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    int divisor, dividend, quotient, remainder;
+
+    cout << "Enter dividend: "<<endl;
+    cin >> dividend;
+
+    cout << "Enter divisor: "<<endl;
+    cin >> divisor;
+
+    quotient = dividend / divisor;
+    remainder = dividend % divisor;
+
+    cout << "Quotient = " << quotient << endl;
+    cout << "Remainder = " << remainder<<endl;
+
+    return 0;
+}
