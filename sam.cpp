723506b6ed14b1ce18b6ce7135d43fd749721c98
SamuelJ22 <Samueljadhav1@gmail.com>
2021-06-04 18:29:36 +0530

    template max

---
+++ sam.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+ 
+// One function works for all data types.  This would work
+// even for user defined types if operator '>' is overloaded
+template <typename T>
+T myMax(T x, T y)
+{
+   int max;
+   if (x > y)
+  return max=x;
+   else 
+  return max=y;
+}
+ 
+int main()
+{
+  cout << myMax<int>(3, 7) << endl;  // Call myMax for int
+  cout << myMax<double>(3.0, 7.0) << endl; // call myMax for double
+  cout << myMax<char>('g', 'e') << endl;   // call myMax for char
+ 
+  return 0;
+}
